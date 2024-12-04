e6dcb967fc8d28077e30bd37033c1aba70f9388a
saidmuhamedovsaidakbar27@gmail.com <Brat711>
2024-12-04 16:48:12 +0500

    asl

---
+++ main.js
@@ -0,0 +1,13 @@
+function updateclock() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('hours').textContent=hours;
+    document.getElementById('minutes').textContent=minutes;
+    document.getElementById('seconds').textContent=seconds;
+
+}
+setInterval(updateclock,1000);
+updateclock();
